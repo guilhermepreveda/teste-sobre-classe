eed4e5ec8db6935e8c4299133a1a03622d487ca3
Guilherme Preveda Milék <102039703+guilhermepreveda@users.noreply.github.com>
2022-06-23 11:36:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ class Loja {
     {
       id: 1,
       nome: "Abajur",
-      preco: 19.9
+      preco: 19.9,
     },
     {
       id: 2,
       nome: "Sofá",
-      preco: 2999.99
+      preco: 2999.99,
     },
     {
       id: 3,
       nome: "Cadeira",
-      preco: 359.9
+      preco: 359.9,
     },
-   ]
+  ];
 }
 
-// Você desenvolve o seu código a partir daqui
+/* ==== Você desenvolve o seu código a partir daqui ============ */
+
+/* ============================================================= */
+/* Valores para usar como parâmetro ao instanciar a classe Pessoa: */
+//   - "Jardel", 123456789
+
+/* Valores para usar como parâmetro ao instânciar a classe Funcionario: */
+//   - "Victor", 987654321, "Móveis"
+
+/* Valores para utilizar o método cadastrarProduto() da classe Funcionario: */
+//   - "Mesa de Jantar", 398.9
+//   - "Pouff", 149.9
+
+console.log("> Os console.log() estão funcionando corretamente! ;)");
+// ex.: const classe = new Classe("parametro1", 2, true, ["a", 1, 2], {chave: "valor"})
