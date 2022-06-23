228bb5512cbf302b616ec6be82c1811f65198395
Guilherme Preveda Milék <102039703+guilhermepreveda@users.noreply.github.com>
2022-06-23 11:09:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+class Loja {
+  static estoque = [
+    {
+      id: 1,
+      nome: "Abajur",
+      preco: 19.9
+    },
+    {
+      id: 2,
+      nome: "Sofá",
+      preco: 2999.99
+    },
+    {
+      id: 3,
+      nome: "Cadeira",
+      preco: 359.9
+    },
+   ]
+}
+
 // Você desenvolve o seu código a partir daqui
